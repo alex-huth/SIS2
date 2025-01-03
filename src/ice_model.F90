5209bb2ac397ff251f4cbe605b8c8fe4bb6d9a05
alex-huth <alexander.huth@noaa.gov>
2025-01-03 16:04:41 -0500

    commented out FATAL error that was called when a cell accumulates calving from both snow discharge (from the land model) and ice shelf calving (from MOM-IS). This is a hack to account for the case where non-ice-shelf calving from the land model (e.g. calving from NE Canada) actually ends up in the same cell as ice-sheet calving (Greenland or Antarctica), due to interpolation from the coarser land grid to the ocean grid. This may need to be addressed more precisely when land-to-ocean stocks are implemented

--- src/ice_model.F90
+++ src/ice_model.F90
@@ -449,7 +449,7 @@ subroutine unpack_ocean_ice_boundary_calved_shelf_bergs(Ice, OIB)
   do j=jsc,jec ; do i=isc,iec ; if (G%mask2dT(i,j) > 0.0) then
     i2 = i+i_off ; j2 = j+j_off
     if (OIB%calving(i2,j2)>0.0) then
-      if (FIA%calving(i,j)>0.0) call SIS_error(FATAL,"Overlap in calving from snow discharge and ice shelf!")
+      !if (FIA%calving(i,j)>0.0) call SIS_error(FATAL,"Overlap in calving from snow discharge and ice shelf!")
       FIA%calving(i,j) = US%kg_m2s_to_RZ_T*OIB%calving(i2,j2)
       FIA%calving_hflx(i,j) = US%W_m2_to_QRZ_T*OIB%calving_hflx(i2,j2)
     endif
